89e4029b865b2dba4b5581c6f7559b29709be2cd
wanlum <295386487@qq.com>
2020-11-23 11:17:03 +0800

    :heavy_plus_sign: 添加composition API插件

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
+import router from "./routers";
 import store from "./store";
 
 // 使用ElementUI
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 Vue.use(ElementUI);
+
+import VueCompositionAPI from '@vue/composition-api'
+
+Vue.use(VueCompositionAPI)
 
 
 Vue.config.productionTip = false;
