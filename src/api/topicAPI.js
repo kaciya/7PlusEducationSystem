8dffd82a0d87b90b63642fffb034b2ba952dadc5
wuhongzhi5 <1035568475@qq.com>
2020-12-12 20:23:46 +0800

    :sparkles:添加柒加圈接口

---
+++ src/api/topicAPI.js
@@ -0,0 +1,11 @@
+// 公告管理接口配置
+export default {
+  // 标签列表
+  getDataList: "/topic/page",
+  // 显示隐藏数据
+  shieldShowUser: "/topic/status",
+  // 获取分类
+  getCategory: "/topic/category/list",
+  // 获取文章详情
+  getArticle: "/topic/detail"
+};
