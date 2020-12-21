3992ff25684461976d405af6e924595957fe26bf
yxd11123123 <yuanxudong.201@qq.com>
2020-12-21 21:04:48 +0800

    🎉 题库上传音频功能组件完成

---
+++ src/components/Question/UploadAudioBtn/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <!-- 上传音频按钮 start -->
+  <a-upload
+    :show-upload-list="false"
+    :action="uploadAudio.url"
+    :headers="uploadAudio.headers"
+    :file-list="uploadAudioList"
+    @change="changeUploadAudio"
+  >
+    <a-button type="primary" size="small" style="margin-left: 10px"
+      >上传音频</a-button
+    >
+  </a-upload>
+  <!-- 上传音频按钮 end -->
+</template>
+
+<script>
+// 引入注入方法
+import { inject } from "vue";
+// 导入上传音频功能
+import { useUploadAudio } from "./useUploadAudio";
+
+export default {
+  props: ["id"],
+  setup(props) {
+    // 获取题目id
+    const { id } = props;
+
+    // 获取父组件的刷新题目列表的方法
+    const getQuestion = inject("getQuestion");
+
+    // 上传音频功能
+    const { uploadAudio, uploadAudioList, changeUploadAudio } = useUploadAudio(
+      id,
+      getQuestion
+    );
+
+    // 返回
+    return {
+      uploadAudio,
+      uploadAudioList,
+      changeUploadAudio,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/Question/UploadAudioBtn/useUploadAudio.js
@@ -0,0 +1,63 @@
+//#region 上传音频
+// 引入响应式API
+import { reactive, ref } from "vue";
+// 导入 post 请求
+import { httpPost } from "@/utils/http";
+// 引入上传音频接口配置
+import questionAPI from "@/api/questionAPI";
+// 引入提示框
+import { message } from "ant-design-vue";
+
+/**
+ * 上传音频
+ * @param {*} id 题目id
+ */
+export function useUploadAudio(id, getQuestion) {
+  // 上传音频
+  const uploadAudio = reactive({
+    // 地址
+    url: '/api' + questionAPI.UploadAudio,
+    // 请求头
+    headers: { Token: window.localStorage.getItem("token") }
+  });
+
+  // 上传音频
+  const uploadAudioList = ref([]);
+
+  // 切换上传音频
+  const changeUploadAudio = info => {
+    let fileList = [...info.fileList];
+    // 限制上传音频数量为 1
+    fileList = fileList.slice(-1);
+    uploadAudioList.value = fileList;
+    // 上传音频完成且成功
+    if (info.file.status == "done" && info.file.response.success) {
+      // 保存音频路径
+      const audioUrl = info.file.response.data.fileUrl;
+      // 将音频路径上传给对应题目
+      httpPost(questionAPI.UploadAudioUrl, {
+        // 题目id
+        id,
+        // 音频路径
+        audioUrl
+      }).then((res) => {
+        if (res.success) {
+          // 提示用户
+          message.success("音频上传成功");
+          // 刷新题目列表
+          getQuestion()
+        }
+      }).catch((err) => {
+        throw err;
+      });
+    }
+  };
+
+  // 导出
+  return {
+    uploadAudio,
+    uploadAudioList,
+    changeUploadAudio
+  };
+}
+//#endregion
