a7722d1a89a0851d6b3ba13ee444aca065954049
yxd11123123 <yuanxudong.201@qq.com>
2021-01-02 18:12:13 +0800

    ✨ HIW的查看和编辑功能完成

---
+++ src/components/Question/HIW/GetHIW/useGetHIW.js
@@ -0,0 +1,34 @@
+//#region 查看hiw题目详情
+// 引入vueAPI
+import { ref, watch } from "vue"
+export function useGetHIW(questionDetail, getModalVisible) {
+  // hiw题目详情
+  const getHIW = ref({})
+
+  // 监听获取的题目详情
+  watch(questionDetail, (val) => {
+    // 获取hiw题目时赋值给getHIW
+    if (getModalVisible.hiw) {
+      getHIW.value = val;
+      let arr = getHIW.value.titleQuestion.split(' ');
+      // 以红色字体展示错误文本
+      getHIW.value.titleText.forEach((titleText) => {
+        arr.forEach((val, index) => {
+          if (val == titleText.answer) {
+            arr[index] = `<span style="color: red">${titleText.text}</span>`
+          }
+        })
+      });
+      getHIW.value.titleQuestion = arr.join(' ');
+      // 处理答案
+      if (getHIW.value.answer) {
+        getHIW.value.answer = getHIW.value.answer.join(', ')
+      }
+    }
+  });
+
+  return {
+    getHIW
+  }
+}
+//#endregion
