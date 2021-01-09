b00c3d2e649f6b4a698c5a7cb9f83f8bba059aa8
fan-sir666 <fan_sir666@163.com>
2021-01-09 11:13:38 +0800

    添加阅读题型FIBR的添加和编辑功能

--- src/components/Question/FIBR/AddFIBR/useAddFIBR.js
+++ src/components/Question/FIBR/AddFIBR/useAddFIBR.js
@@ -17,15 +17,25 @@ export function useAddFIBR(addModalVisible, getQuestion) {
       title: "",
       // 标签选择
       labelIds: [],
+      // 题目原文
+      titleText: ["", ""],
       // 题目选项
       choices: [
         {
           content: "",
-          key: "1"
+          key: "A"
         },
         {
           content: "",
-          key: "2"
+          key: "B"
+        },
+        {
+          content: "",
+          key: "C"
+        },
+        {
+          content: "",
+          key: "D"
         }
       ],
       // 答案参考
@@ -68,36 +78,114 @@ export function useAddFIBR(addModalVisible, getQuestion) {
       message.warn("每题标签最多可以选择三个");
     }
   };
+  // 添加填空
+  const addCalking = () => {
+    addFIBR.model.titleText.push("");
+  };
+  const delCalking = index => {
+    addFIBR.model.titleText.splice(index, 1);
+  };
   // 添加题目选项
   const addChoices = () => {
     addFIBR.model.choices.push({
       content: "",
       // A、B、C、D...
-      key: addFIBR.model.choices.length + 1
+      key: String.fromCharCode(addFIBR.model.choices.length + 65)
     });
   };
 
   // 删除题目选项
-  const delChoices = index => {
-    addFIBR.model.choices.splice(index, 1);
+  const delChoices = () => {
+    addFIBR.model.choices.pop();
     // 重置一下选项答案
     addFIBR.model.answer = [];
   };
   // 添加FIBR题目
   const confirmAddFIBR = () => {
-    console.log(addFIBR.model);
-    // 关闭FIBR模态框
-    addModalVisible.fibr = false;
+    // console.log(addFIBR.model);
+    // 先校验
+    addFIBRRef.value
+      .validate()
+      .then(() => {
+        // 发送添加题目请求
+        httpPost(read.AddQuestion("fibr"), addFIBR.model)
+          .then(res => {
+            console.log(res);
+            if (res.success == true) {
+              // 提示用户添加成功
+              message.success("添加题目成功");
+              // 刷新页面
+              getQuestion();
+              // 关闭mcm模态框
+              addModalVisible.fibr = false;
+              // 重置表单
+              addFIBRRef.value.resetFields();
+              // 因为没有对应的name 所以需要手动重置
+              addFIBR.model.choices = [
+                {
+                  content: "",
+                  key: "A"
+                },
+                {
+                  content: "",
+                  key: "B"
+                },
+                {
+                  content: "",
+                  key: "C"
+                },
+                {
+                  content: "",
+                  key: "D"
+                }
+              ];
+              addFIBR.model.titleText = ["", ""];
+            } else {
+              // 添加失败，提示用户失败原因
+              message.error(res.message);
+            }
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      })
+      .catch(err => {
+        console.log(err);
+      });
   };
   // 取消添加FIBR题目
   const cancelAddFIBR = () => {
     // 提示用户
     message.warn("取消添加fibr题目");
+    // 重置表单
+    addFIBRRef.value.resetFields();
+    // 因为没有对应的name 所以需要手动重置
+    addFIBR.model.choices = [
+      {
+        content: "",
+        key: "A"
+      },
+      {
+        content: "",
+        key: "B"
+      },
+      {
+        content: "",
+        key: "C"
+      },
+      {
+        content: "",
+        key: "D"
+      }
+    ];
+    addFIBR.model.titleText = ["", ""];
   };
   return {
     addFIBR,
     addFIBRRef,
     changeLabels,
+    addCalking,
+    delCalking,
     addChoices,
     delChoices,
     confirmAddFIBR,
--- src/components/Question/FIBR/EditFIBR/index.vue
+++ src/components/Question/FIBR/EditFIBR/index.vue
@@ -7,28 +7,171 @@
     @ok="confirmEditFIBR"
     @cancel="cancelEditFIBR"
   >
-    <p>我是编辑模态框FIBR</p>
+    <!-- 编辑ro题目表单 start -->
+    <a-form
+      :model="editFIBR.model"
+      :rules="editFIBR.rules"
+      ref="editFIBRRef"
+      :label-col="{ span: 4 }"
+      :wrapper-col="{ span: 18 }"
+    >
+      <!-- 编号 -->
+      <a-form-item label="编号" name="no" hasFeedback>
+        <a-input v-model:value="editFIBR.model.no" />
+      </a-form-item>
+      <!-- 题目 -->
+      <a-form-item label="题目" name="title" hasFeedback>
+        <a-input v-model:value="editFIBR.model.title" />
+      </a-form-item>
+      <!-- 标签复选框 -->
+      <a-form-item label="标签选择" name="labelIds">
+        <a-checkbox-group
+          v-model:value="editFIBR.model.labelIds"
+          @change="changeLabels"
+        >
+          <a-checkbox :value="item.id" v-for="item in labelList" :key="item.id">
+            {{ item.name }}
+          </a-checkbox>
+        </a-checkbox-group>
+      </a-form-item>
+      <!-- 题目原文 start -->
+      <span class="install">题目原文：</span>
+      <a-form-item
+        label=" "
+        :colon="false"
+        v-for="(item, index) in editFIBR.model.titleText"
+        :key="index"
+      >
+        <a-input
+          v-model:value="editFIBR.model.titleText[index]"
+          style="width: 68%"
+        />
+        <a-select
+          v-if="index != editFIBR.model.titleText.length - 1"
+          style="width: 20%; margin: 0 10px"
+          v-model:value="editFIBR.model.answer[index]"
+        >
+          <a-select-option
+            v-for="item in editFIBR.model.choices"
+            :key="item.key"
+            :value="item.key"
+          >
+            {{ item.key }}
+          </a-select-option>
+        </a-select>
+        <MinusCircleOutlined
+          v-if="index != 0 && index != editFIBR.model.titleText.length - 1"
+          @click="delCalking(index)"
+        />
+      </a-form-item>
+      <a-form-item label=" " :colon="false">
+        <a-button type="dashed" style="width: 60%" @click="editCalking">
+          <PlusOutlined />添加填空
+        </a-button>
+      </a-form-item>
+      <!-- 题目原文 end -->
+      <!-- 参考答案 -->
+      <a-form-item label="参考答案：" name="answer">
+        <a-tag v-for="(item, index) in editFIBR.model.choices" :key="item.key"
+          ><span style="margin-right: 8px">{{ item.key + " :" }}</span
+          ><a-input
+            style="width: 80%"
+            v-model:value="editFIBR.model.choices[index].content"
+        /></a-tag>
+      </a-form-item>
+      <a-form-item label=" " :colon="false">
+        <a-button
+          type="dashed"
+          style="width: 40%; margin-right: 30px"
+          @click="editChoices"
+        >
+          <PlusOutlined />添加选项
+        </a-button>
+        <a-button type="dashed" style="width: 40%" @click="delChoices">
+          <MinusCircleOutlined />移除选项
+        </a-button>
+      </a-form-item>
+      <a-form-item label="解析" name="titleAnalysis">
+        <a-textarea v-model:value="editFIBR.model.titleAnalysis" :rows="4" />
+      </a-form-item>
+      <a-form-item label="备注" name="remark">
+        <a-textarea v-model:value="editFIBR.model.remark" :rows="2" />
+      </a-form-item>
+    </a-form>
   </a-modal>
 </template>
 
 <script>
+// 引入注入方法
+import { inject } from "vue";
+// 引入图标
+import { MinusCircleOutlined, PlusOutlined } from "@ant-design/icons-vue";
 // 引入 编辑FIBR题目 功能
 import { useEditFIBR } from "./useEditFIBR";
+// 引入 标签列表 功能
+import { useGetLabels } from "@/views/Question/QuestionLabel/useGetLables";
 export default {
   props: ["editModalVisible"],
   setup(props) {
     // 编辑模态框的显示与隐藏
     const { editModalVisible } = props;
+    // 获取父组件的刷新题目列表的方法
+    const getQuestion = inject("getQuestion");
+    // 获取要编辑的题目详情
+    const editDetail = inject("questionDetail");
+    // 标签列表
+    const { labelList } = useGetLabels();
     // 编辑FIBR题目
-    const { confirmEditFIBR, cancelEditFIBR } = useEditFIBR(editModalVisible);
+    const {
+      editFIBR,
+      editFIBRRef,
+      changeLabels,
+      editCalking,
+      delCalking,
+      editChoices,
+      delChoices,
+      confirmEditFIBR,
+      cancelEditFIBR,
+    } = useEditFIBR(editModalVisible, editDetail, getQuestion);
     return {
+      // 标签列表
+      labelList,
+      editFIBR,
+      editFIBRRef,
+      changeLabels,
+      editCalking,
+      delCalking,
+      editChoices,
+      delChoices,
       // 编辑FIBR题目
       confirmEditFIBR,
       // 取消编辑FIBR题目
       cancelEditFIBR,
     };
   },
+  components: {
+    // 删除选项按钮
+    MinusCircleOutlined,
+    // 添加选项按钮
+    PlusOutlined,
+  },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.ant-form {
+  position: relative;
+}
+.install {
+  position: absolute;
+  top: 200px;
+  left: 12px;
+  color: rgba(0, 0, 0, 0.85);
+}
+.ant-tag {
+  width: 40%;
+  background-color: #fff;
+  font-weight: 700;
+  border: none;
+}
+</style>
