bb8ca8f7d78999e5d5a685abb6a5c3ef41f672ea
wanlum <295386487@qq.com>
2021-01-13 23:32:20 +0800

    update: ant-design-vue 按需加载

--- src/main.js
+++ src/main.js
@@ -6,8 +6,64 @@ import store from "./store";
 const app = createApp(App);
 
 // 使用Ant Design Vue
-import Antd from "ant-design-vue";
+import {
+  Button,
+  ConfigProvider,
+  Layout,
+  Grid,
+  Row,
+  Col,
+  Input,
+  Form,
+  Switch,
+  Tooltip,
+  Menu,
+  Avatar,
+  Modal,
+  Dropdown,
+  Card,
+  PageHeader,
+  Breadcrumb,
+  Radio,
+  Table,
+  Popconfirm,
+  Select,
+  Upload,
+  Alert,
+  Tag,
+  Checkbox,
+  Divider,
+  DatePicker
+} from "ant-design-vue";
+app.use(Button);
+app.use(Grid);
+app.use(Row);
+app.use(Col);
+app.use(ConfigProvider);
+app.use(Layout);
+app.use(Input);
+app.use(Switch);
+app.use(Tooltip);
+app.use(Form);
+app.use(Menu);
+app.use(Avatar);
+app.use(Dropdown);
+app.use(Card);
+app.use(PageHeader);
+app.use(Breadcrumb);
+app.use(Radio);
+app.use(Modal);
+app.use(Table);
+app.use(Popconfirm);
+app.use(Select);
+app.use(Upload);
+app.use(Alert);
+app.use(Tag);
+app.use(Checkbox);
+app.use(Divider);
+app.use(DatePicker)
 import "ant-design-vue/dist/antd.css";
+
 import "@/assets/styles/index.scss"; // global css
 
 // 全局配置message.config
@@ -33,6 +89,5 @@ app.provide("$echarts", echarts);
 app
   .use(store)
   .use(router)
-  .use(Antd)
   .use(CKEditor)
   .mount("#app");
