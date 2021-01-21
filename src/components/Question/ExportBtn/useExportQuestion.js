e32369845b0deeddd00d9cdf152fcab4ee4bcc65
yxd11123123 <yuanxudong.201@qq.com>
2021-01-21 10:56:46 +0800

    🐛 处理导出失败请况

--- src/components/Question/ExportBtn/useExportQuestion.js
+++ src/components/Question/ExportBtn/useExportQuestion.js
@@ -29,20 +29,25 @@ export function useExportQuestion(questionType) {
       // 二级制流文件，一定要设置成blob
       responseType: "blob"
     }).then(res => {
-      //创建应该a标签
-      const link = document.createElement("a");
-      const blob = new Blob([res.data], { type: "application/vnd.ms-excel" });
-      // 隐藏a标签
-      link.style.display = "none";
-      link.href = URL.createObjectURL(blob);
-      // 设置文件名
-      link.setAttribute("download", `${category.toUpperCase()}题目.xlsx`);
-      // 添加a标签
-      document.body.appendChild(link);
-      // 点击
-      link.click();
-      // 删除a标签
-      document.body.removeChild(link);
+      if (res.status == 200) {
+        //创建应该a标签
+        const link = document.createElement("a");
+        const blob = new Blob([res.data], { type: "application/vnd.ms-excel" });
+        // 隐藏a标签
+        link.style.display = "none";
+        link.href = URL.createObjectURL(blob);
+        // 设置文件名
+        link.setAttribute("download", `${category.toUpperCase()}题目.xlsx`);
+        // 添加a标签
+        document.body.appendChild(link);
+        // 点击
+        link.click();
+        // 删除a标签
+        document.body.removeChild(link);
+      }
+      else {
+        message.error(res.data);
+      }
     }).catch((err) => {
       if (err.response.status == 403) {
         message.error("没有访问权限")
